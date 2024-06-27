aef957525af948291db97d85304d892c98722be5
michellevanderlinde <2747268@gmail.com>
2024-06-27 14:50:20 +0200

    Initial commit docker compose and init files for database

---
+++ init/onebankcpt_db/init.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS `obcpt_db`;
+GRANT ALL ON `obcpt_db`.* TO 'user'@'%';
---
+++ init/onebankhub_db/init.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS `onebankhub_db`;
+GRANT ALL ON `onebankhub_db`.* TO 'user'@'%';
